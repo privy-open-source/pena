2f253d7d63c4caea404fe7a7e1d55f81740ddc7f
Ade Novid <adenvt@gmail.com>
2023-02-20 14:17:38 +0700

    refactor(vue): build to esm

--- packages/pena-vue/build.config.ts
+++ packages/pena-vue/build.config.ts
@@ -2,13 +2,7 @@ import { defineBuildConfig } from 'unbuild'
 
 export default defineBuildConfig({
   entries: [
-    { builder: 'mkdist', input: './src/' },
-    {
-      builder: 'mkdist',
-      input  : './src/',
-      format : 'cjs',
-      ext    : 'js',
-    },
+    'src/'
   ],
   declaration: true,
   rollup     : { emitCJS: true, cjsBridge: true },
